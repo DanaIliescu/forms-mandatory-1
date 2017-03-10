c24adbf25bde7a0d6c6818cb72a85457f7096d83
Dana Iliescu <dana.iliescu96@gmail.com>
2017-03-10 17:50:35 +0100

    Error messages displayed

--- src/app/air-form/air-form.component.ts
+++ src/app/air-form/air-form.component.ts
@@ -16,27 +16,32 @@ export class AirFormComponent{
       'destinationCity': ['', Validators.compose([Validators.required, this.cityValidator])],
       'twoWay': [false, Validators.required],
       'departureDate': ['', Validators.required],
-      'returnDate': ['', ],
-      'passengers': [1, Validators.required]
+      'returnDate': [''],
+      'passengers': [1, Validators.compose([Validators.required, this.passengersValidator])]
     });
   }
 
-  // Create a bunch of custom validators
-  // every city needs to start with a uppercase adn then contain just lowercase letters
   cityValidator(control: FormControl): {[s: string]: boolean}{
       if(!control.value.match(/^[A-Z]{1}[a-z]+([ ][A-Z]{1}[a-z]+)*$/)){
-        return {invalidCity: true};
+        return {'cityError': true};
       }
       return null;
+  }
+
+  passengersValidator(control: FormControl): {[s: string]: boolean}{
+    if(!String(control.value).match(/^(0|[1-9][0-9]*)$/)){
+      return {invalidPassengers: true};
+    }
+    return null;
   }
 
   onSubmit(): void {
     if (this.mainForm.valid) {
       console.log("Tak");
     }else {
-      console.log("the form is not valid!");
+      console.log("The form is not valid!");
     }
-    console.log("You submitted the value: ",this.mainForm.value);
+    console.log("You submitted the value: ", this.mainForm.value);
+    console.log(this.mainForm);
   }
-
 }
