8eab342ce69cb697af38d77fa63a7d754d76fbea
Andrei Dogaru <doggy@Andreis-MacBook-Pro.local>
2017-03-10 14:56:33 +0100

    validation added

--- src/app/air-form/air-form.component.ts
+++ src/app/air-form/air-form.component.ts
@@ -7,28 +7,36 @@ import { FormGroup, FormBuilder, Validators,
   templateUrl: './air-form.component.html',
   styleUrls: ['./air-form.component.css']
 })
-export class AirFormComponent implements OnInit {
-  private mainForm: FormGroup;
+export class AirFormComponent{
+  mainForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
-
-  ngOnInit() {
-    this.mainForm = this.formBuilder.group({
-      departureCity: ['', Validators.required],
-      destinationCity: ['', Validators.required],
-      twoWay: [false, Validators.required],
-      departureDate: ['', Validators.required],
-      returnDate: ['', ],
-      passengers: [1, Validators.required]
+  constructor(formBuilder: FormBuilder) {
+    this.mainForm = formBuilder.group({
+      'departureCity': ['', Validators.compose([Validators.required, this.cityValidator])],
+      'destinationCity': ['', Validators.compose([Validators.required, this.cityValidator])],
+      'twoWay': [false, Validators.required],
+      'departureDate': ['', Validators.required],
+      'returnDate': ['', ],
+      'passengers': [1, Validators.required]
     });
+  }
+
+  // Create a bunch of custom validators
+  // every city needs to start with a uppercase adn then contain just lowercase letters
+  cityValidator(control: FormControl): {[s: string]: boolean}{
+      if(!control.value.match(/^[A-Z]{1}[a-z]+([ ][A-Z]{1}[a-z]+)*$/)){
+        return {invalidCity: true};
+      }
+      return null;
   }
 
   onSubmit(): void {
     if (this.mainForm.valid) {
-      console.log("submitted", this.mainForm.value);
+      console.log("Tak");
+    }else {
+      console.log("the form is not valid!");
     }
-
-    else console.log("the form is not valid!");
+    console.log("You submitted the value: ",this.mainForm.value);
   }
 
 }
